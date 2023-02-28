29325c711149426e9f0abd359e5760a78eebe87b
Austin Sonderman <6412733+sondy91@users.noreply.github.com>
2023-02-27 19:38:34 -0600

    Getting VuePrime components working

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useToast } from "primevue/usetoast";
+const text = ref();
+const toast = useToast();
+const greet = () => {
+  toast.add({severity:'success', summary: 'PrimeTime', detail: text.value });
+}
+</script>
+
+<template>
+  <Toast></Toast>
+  <div class="container">
+    <span class="p-float-label">
+      <InputText id="txt" type="text" v-model="text" />
+      <label for="txt">Text</label>
+    </span>
+    <Button label="Greet" @click="greet" icon="pi pi-user"></Button>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config';
+
+//PrimeVue components
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+//PrimeVue Themes
+import 'primevue/resources/themes/tailwind-light/theme.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+
+import 'primeicons/primeicons.css';
+
+const app = createApp(App)
+
+app.use(PrimeVue);
+app.use(createPinia())
+app.use(router)
+app.use(ToastService);
+
+
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Toast', Toast);
+
+app.mount('#app')
